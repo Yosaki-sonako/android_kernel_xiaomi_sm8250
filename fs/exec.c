f98068a46437310ab35b04589cc69c0c5e733a18
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-03 14:00:56 -0800

    kernel: Boost to the max for a short amount of time when zygote forks
    In order to boost during the period from when a new app is forked to the
    time that it is added to the top-app cpuset (which is a signal to
    subsequently boost again), boost for a short amount of time when zygote
    forks. Not all processes forked from zygote are top apps, hence the
    decoupled boosting setup.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [0ctobot: Adapted for msm-4.14]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -111,6 +111,16 @@ void dead_special_task(void)
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define ZYGOTE32_BIN "/system/bin/app_process32"
+#define ZYGOTE64_BIN "/system/bin/app_process64"
+static struct signal_struct *zygote32_sig;
+static struct signal_struct *zygote64_sig;
+
+bool task_is_zygote(struct task_struct *p)
+{
+	return p->signal == zygote32_sig || p->signal == zygote64_sig;
+}
+
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
 	BUG_ON(!fmt);
@@ -1894,6 +1904,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 		goto out;
 
 	if (is_global_init(current->parent)) {
+
 		if (unlikely(!strcmp(filename->name, LIBPERFMGR))) {
 			WRITE_ONCE(powerhal_tsk, current);
                 } else if (unlikely(!strcmp(filename->name, LIBPERFMGR_BIN))) {
@@ -1905,7 +1916,14 @@ static int __do_execve_file(int fd, struct filename *filename,
 		} else if (unlikely(!strcmp(filename->name, SERVICEMANAGER_BIN))) {
 			WRITE_ONCE(servicemanager_tsk, current);
 		}
+
+		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
+			zygote32_sig = current->signal;
+		} else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN))) {
+			zygote64_sig = current->signal;
+
 	}
+}
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;
