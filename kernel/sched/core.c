9fe3b5a9a65cb57a02908e3529479ca3b2548dce
utziacre <utziacre@gmail.com>
2023-11-22 13:46:51 +0800

    sched/core: Introduce Uclamp Assist

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -769,7 +769,8 @@ unsigned int sysctl_sched_uclamp_util_max = SCHED_CAPACITY_SCALE;
  * above.
  */
 
-unsigned int sysctl_sched_uclamp_util_min_rt_default = SCHED_CAPACITY_SCALE;
+unsigned int sysctl_sched_uclamp_util_min_rt_default = 0;
+
 
 
 /* All clamps are required to be less or equal than these values */
@@ -8277,10 +8278,11 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 	int i;
 
 	static struct uclamp_param tgts[] = {
-		{"top-app",             "0", "max",  1},
-       		{"foreground",          "0",  "80",  0},
-                {"dex2oat",             "0",  "30",  0},
-        	{"background",          "0",  "30",  0},
+		{"top-app",             "1", "max",  1},
+       		{"foreground",          "0", "max",  0},
+                {"dex2oat",             "0",  "60",  0},
+        	{"background",          "0",  "50",  0},
+
         	{"system-background",   "0",  "50",  0},
 	};
 
@@ -8300,7 +8302,9 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 
 			pr_info("uclamp_assist: setting values for %s: uclamp_min=%s uclamp_max=%s uclamp_latency_sensitive=%d\n",
 				tgt.name, tgt.uclamp_min, tgt.uclamp_max,tgt.uclamp_latency_sensitive);
-			return;
+
+			return;			
+
 		}
 	}
 
